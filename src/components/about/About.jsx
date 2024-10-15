eaad7c24af4ffadc4e069c7b0df47d4f1cfe68b1
Nate Loken <nfloken@gmail.com>
2024-10-15 14:40:36 -0700

    Updated about and resume to match current experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,71 +5,54 @@ import { FaAward } from 'react-icons/fa'
 
 
 const About = () => {
-  return (
-    <section id='about'>
-      <h5>Get To Know</h5>
-      <h2>About me</h2>
+    return (
+        <section id='about'>
+        <h5>Get To Know</h5>
+        <h2>About me</h2>
 
-      <div className='container about__container'>
-        <div className='about__me'>
-          <div className='about__me-image'>
-            <img src={Nate_Grad16} alt='about' />
-          </div>
-        </div>
-        <div className='about__content'>
-          <div className='about__cards'>
-            <article className='about__card'>
-              <FaAward className='about__icon' />
-              <h5>Exprience</h5>
-              <small>3 Years Experience</small>
-            </article>
+        <div className='container about__container'>
+            <div className='about__me'>
+                <div className='about__me-image'>
+                    <img src={Nate_Grad16} alt='about' />
+                </div>
+                </div>
+                <div className='about__content'>
+                    <div className='about__cards'>
+                        <article className='about__card'>
+                            <FaAward className='about__icon' />
+                            <h5>Exprience</h5>
+                            <small>3 Years Experience</small>
+                        </article>
+                </div>
+                <br />
 
-            {/* <article className='about__card'> */}
-            {/*   <VscFolderLibrary className='about__icon' /> */}
-            {/*   <h5>Projects</h5> */}
-            {/*   <small>My Projects</small> */}
-            {/* </article> */}
-          </div>
-          <br />
+                <p>
+                    <h4>Hello my Name is Nathaniel Loken.</h4>
+                    <br />
+                    I am a dedicated Software Engineer seeking opportunities to leverage my expertise in building innovative software solutions that make a global impact. I am passionate about working in dynamic environments that encourage learning, collaboration, and continuous growth.
+                    <br />
+                    <br />
+                    During my internship with NASA, I gained hands-on experience in test-driven development, continuous integration, and Agile practices. I developed a comprehensive test suite, created CI/CD pipelines using GitLab, and built an endpoint for file validation and database 
+                    integration—contributing to efficient project development and deployment.
+                    <br />
+                    <br />
 
-          <p>
-            <h4>Hello my Name is Nathaniel Loken.</h4>
-            <br />
-            I am seeking a career in Software Engineering where I can leverage my
-            skills to innovate with emerging technologies that make a positive impact
-            globally. I thrive in environments where I can meet new people, continuously
-            learn, and develop my abilities.
-            <br />
-            <br />
-            During my internship with NASA, I acquired valuable skills such as
-            test-driven development, continuous integration and deployment, and
-            working in an AGILE development environment. I created the test suite
-            and continuous integration scripts for our project, developed an
-            endpoint to validate and upload files to the database, and set up the CI/CD pipeline using GitLab.
-            <br />
-            <br />
-            Previously, I worked as a Fullstack Engineer at the Mental Health Wellness Center, 
-            where I developed a Learning Management System. This platform helps
-            users learn about topics like mindfulness and depression at their own pace.
-            For this project, I used Python and Django for the backend and Vue.js for the frontend,
-            integrating other technologies like Stripe for payment fulfillment.
-            <br />
-            <br />
-            Currently, I am a Software Team Lead at Raytheon, overseeing the embedded software section.
-            In this role, I lead a team of engineers in developing and maintaining
-            software for various embedded systems, ensuring high performance and reliability.
-            My responsibilities include project management, code review, and mentorship,
-            as well as hands-on development to solve complex technical challenges.
-            This position has honed my leadership skills and deepened my expertise in embedded systems and software engineering.
-          </p>
+                    Following that, as a Full-stack Engineer at the Mental Health Wellness Center, I developed an online Learning Management System to deliver life coaching content. Using Python, Django for the backend, and Vue.js for the frontend, I integrated payment functionality 
+                    through Stripe to create a seamless user experience.
+                    <br />
+                    <br />
 
-          <a href='#contact' className='btn btn-primary'>
-            Let's Talk
-          </a>
-        </div>
-      </div>
-    </section>
-  )
+                    Currently, I am an Embedded Software Engineer at Raytheon, leading a team in the development and maintenance of embedded weapon software. I manage all aspects of the project lifecycle, from design and implementation to testing in various environments. 
+                    My role has sharpened my leadership, technical problem-solving, and project management skills, particularly in the embedded systems domain.
+                </p>
+
+                <a href='#contact' className='btn btn-primary'>
+                    Let's Talk
+                </a>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default About
